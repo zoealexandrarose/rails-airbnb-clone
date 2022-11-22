4f5e8dcf629585d82103eb0b8260436df2471476
Zoe Rose zoealexandrarose@outlook.com <zoealexandrarose@outlook.com>
2022-11-22 12:02:57 +0100

    seeds done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Create users
+Product.destroy_all
+puts "Creating products..."
+
+# User.create!(first_name: "Danielle", last_name: "Rose", email: 'danielle@danielle.com', password: "123456")
+
+User.all.each do |user|
+  3.times.each do
+    Product.create!(user: user, name: "Moonshine - Skateboard", description: "Beatuiful skateboard from the 90s", price: 20)
+  end
+end
+
+puts "Done!"
